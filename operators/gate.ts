0795d66abc49ea06b669ead121332e1c53b9c65b
pestras <admin@pestras.com>
2020-06-24 20:57:52 +0300

    fix gate oprator

--- operators/gate.ts
+++ operators/gate.ts
@@ -1,13 +1,16 @@
-import { Observable, BehaviorSubject, combineLatest } from "rxjs";
-import { filter, map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { filter, map, switchMap, distinctUntilChanged } from "rxjs/operators";
 
-export function gate<T>(guard: Observable<boolean>, invert = false) {
-
-  return function (source: Observable<T>) {
-    return combineLatest(source, guard)
-      .pipe(
-        filter(([, open]) => invert ? !open : open),
-        map(([value]) => value)
+export function gate<T>(controller: Observable<boolean>, invert = false) {
+  return function(source: Observable<T>) {
+    return source.pipe(
+      switchMap(
+        val => controller.pipe(
+          filter(open => (invert ? !open : open)),
+          distinctUntilChanged(),
+          map(() => val)
+        )
       )
-  }
-} 
+    );
+  };
+}
